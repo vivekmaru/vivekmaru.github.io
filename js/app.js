c2cca1e108035bb49be4caf8f66f9399a148acd4
Vivek Maru <vivek.maru87@gmail.com>
2018-01-28 21:27:44 +1100

    Added form validation and Ajax

--- js/app.js
+++ js/app.js
@@ -4,37 +4,39 @@
  * If you have any feedback please let me know!
  */
 
-(function(){
+(function () {
 
-    var model = {
+    const model = {
         skillCounter: 0,
         navToggled: false,
     }
 
-    var app = {
+    const app = {
 
-        init: function(){
+        init: function () {
             this.cacheDOM();
             this.bindEvents();
             this.toggleScroll();
             this.showSkill(model.skillCounter);
         },
 
-        cacheDOM: function(){
-            this.$skill      = document.getElementsByClassName('skill');
+        cacheDOM: function () {
+            this.$skill = document.getElementsByClassName('skill');
             this.$skillArrow = $('.slider-arrow');
             this.$navOverlay = $('.nav-overlay');
-            this.$toggleNav  = $('.toggle-nav');
+            this.$toggleNav = $('.toggle-nav');
+            this.$formSubmit = $('#submit');
         },
 
-        bindEvents: function(){
+        bindEvents: function () {
             this.$skillArrow.on('click', this.skillSlider.bind(this));
             this.$toggleNav.on('click', this.toggleNav.bind(this));
+            this.$formSubmit.on('click', this.validateForm);
             $(window).scroll(this.toggleScroll);
             $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(this.smoothScroll);
         },
 
-        toggleScroll: function(){
+        toggleScroll: function () {
             if ($(document).scrollTop() > 0) {
                 $('nav').addClass('nav-scroll');
             } else {
@@ -42,7 +44,7 @@
             }
 
             var scrollBarLocation = $(this).scrollTop();
-            $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').each(function(){
+            $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').each(function () {
                 var sectionOffset = $(this.hash).offset().top;
                 if (Math.floor(sectionOffset) <= scrollBarLocation) {
                     $(this).addClass('active');
@@ -53,7 +55,7 @@
             });
         },
 
-        showSkill: function(j){
+        showSkill: function (j) {
             var length = this.$skill.length;
             for (var i = 0; i < length; i++) {
                 $(this.$skill[i]).hide();
@@ -61,7 +63,56 @@
             $(this.$skill[j]).show();
         },
 
-        skillSlider: function(e){
+        validateForm: function (e) {
+            var error = false;
+            const fields = {
+                name: $('#name'),
+                email: $('#email'),
+                message: $('#message')
+            };
+            if (fields.name.val() == "") {
+                console.log(fields.name.addClass);
+                fields.name.addClass("error");
+                error = true;
+            }
+            if (fields.email.val() == "") {
+                fields.email.addClass("error");
+                error = true;
+            } else {
+                var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+                if (!reg.test(fields.email.val())) {
+                    fields.email.addClass("error");
+                    error = true;
+                }
+            }
+            if (fields.message.val() == "") {
+                fields.message.addClass("error");
+                error = true;
+            }
+            if(!error) {
+                $.ajax({
+                    type: $("form").attr("method"),
+                    url: $("form").attr("action"),
+                    data: "name=" + $("#name").val() + "&email=" + $("#email").val() + "&message=" + $("#message").val().replace('&','%26'),
+                    dataType: "json",
+                    beforeSend: function() {
+                        $("#status button").hide();
+                    },
+                    success: function(html) {
+                        $("#status #success").show();
+                        console.log(html);
+                    },
+                    error: function(html) {
+                        $("#status button").show();
+                        //$("#status #error").html(html).show();
+                    }
+                });
+            }
+            console.log(this);
+            e.preventDefault();
+        },
+
+        skillSlider: function (e) {
             var i = $(e.target).data('value');
             model.skillCounter += Number(i);
             if (model.skillCounter < 0) {
@@ -70,7 +121,7 @@
             this.showSkill(model.skillCounter % this.$skill.length);
         },
 
-        toggleNav: function(){
+        toggleNav: function () {
             if (model.navToggled) {
                 this.$navOverlay.css('width', '0%');
                 model.navToggled = false;
@@ -80,9 +131,9 @@
             }
         },
 
-        smoothScroll: function(e){
+        smoothScroll: function (e) {
             e.preventDefault();
-            (model.navToggled) ? app.toggleNav() : '';
+            (model.navToggled) ? app.toggleNav(): '';
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top
             }, 1000);
